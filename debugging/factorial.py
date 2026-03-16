a1c8986ca796e654b901036e2565df42d168d9eb
asapelcin <13572@holbertonstudents.com>
2026-03-16 15:57:58 +0400

    Fix infinite loop in factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,7 +5,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Bu sətir mütləq olmalıdır, yoxsa proqram donur
+        n -= 1 
     return result
 
 if len(sys.argv) > 1:
